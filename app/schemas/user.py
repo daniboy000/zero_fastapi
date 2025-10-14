340c3e7f25d5d6b094cad8e532f6433d082334f4
Daniel de Aguiar <daniel.bdaguiar@gmail.com>
2025-10-13 22:43:51 -0300

    Create new User class

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -1,12 +1,20 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 
 
 class UserSchema(BaseModel):
     username: str
-    email: str
+    email: EmailStr
     password: str
 
 
 class UserPublic(BaseModel):
     username: str
-    email: str
+    email: EmailStr
+
+
+class UserDB(UserSchema):
+    id: int
+
+
+class UserList(BaseModel):
+    users: list[UserPublic]
