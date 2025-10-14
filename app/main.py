5f1de7512c809a6284aa3f304c015b809a9e77e0
Daniel de Aguiar <daniel.bdaguiar@gmail.com>
2025-10-13 21:52:57 -0300

    update ide files

--- app/main.py
+++ app/main.py
@@ -1,8 +1,18 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+
+from app.schemas.message import Message
+from app.schemas.user import UserPublic, UserSchema
 
 app = FastAPI()
 
 
-@app.get('/')
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Hello World'}
+
+
+@app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
+def create_user(user: UserSchema):
+    pass
